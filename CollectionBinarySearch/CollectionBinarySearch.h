d8df076bb54b153c4ad5ba697a94d05717d82815
Jonathan Crooke <joncrooke@gmail.com>
2016-12-02 17:00:03 +0100

    Initial Commit

---
+++ CollectionBinarySearch/CollectionBinarySearch.h
@@ -0,0 +1,19 @@
+//
+//  CollectionBinarySearch.h
+//  CollectionBinarySearch
+//
+//  Created by Jonathan Crooke on 02/12/2016.
+//  Copyright © 2016 Jonathan Crooke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CollectionBinarySearch.
+FOUNDATION_EXPORT double CollectionBinarySearchVersionNumber;
+
+//! Project version string for CollectionBinarySearch.
+FOUNDATION_EXPORT const unsigned char CollectionBinarySearchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CollectionBinarySearch/PublicHeader.h>
+
+

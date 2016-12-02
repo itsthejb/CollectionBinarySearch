d391ffa619d6c20b047194ed33b182f301c77938
Jonathan Crooke <joncrooke@gmail.com>
2016-12-02 18:57:53 +0100

    Travis osx complete

--- CollectionBinarySearch/CollectionBinarySearch.h
+++ CollectionBinarySearch/CollectionBinarySearch.h
@@ -6,14 +6,10 @@
 //  Copyright © 2016 Jonathan Crooke. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CollectionBinarySearch.
 FOUNDATION_EXPORT double CollectionBinarySearchVersionNumber;
 
 //! Project version string for CollectionBinarySearch.
 FOUNDATION_EXPORT const unsigned char CollectionBinarySearchVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <CollectionBinarySearch/PublicHeader.h>
-
-

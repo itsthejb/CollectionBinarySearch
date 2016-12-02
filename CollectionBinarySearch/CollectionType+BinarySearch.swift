984449cd812cfbbe467739f9f20b5826708c02b6
Jonathan Crooke <joncrooke@gmail.com>
2016-12-02 17:05:18 +0100

    Initial

---
+++ CollectionBinarySearch/CollectionType+BinarySearch.swift
@@ -0,0 +1,133 @@
+import Foundation
+
+extension Collection
+  where
+  Iterator.Element: Comparable,
+  Iterator.Element: Equatable,
+  Index == Int,
+  IndexDistance == Int
+{
+  typealias Comparator = (Self.Iterator.Element) -> Bool
+
+  func indexOfObject(
+    _ object: Self.Iterator.Element,
+    option: NSBinarySearchingOptions = NSBinarySearchingOptions.firstEqual) -> Index?
+  {
+    return indexOfObject(equality: { $0 == object }, greaterThan: { $0 > object }, lessThan: { $0 < object })
+  }
+
+  func indexOfObject(
+    _ option: NSBinarySearchingOptions = NSBinarySearchingOptions.firstEqual,
+    equality: Comparator,
+    greaterThan: Comparator,
+    lessThan: Comparator
+    ) -> Index?
+  {
+    if option == NSBinarySearchingOptions.insertionIndex {
+      return indexOfInsertion(equality, greaterThan: greaterThan, lessThan: lessThan)
+    } else {
+      return indexOfBoundedObject(option, equality: equality, greaterThan: greaterThan)
+    }
+  }
+
+  // MARK: Default
+
+  fileprivate func indexOfBoundedObject(
+    _ object: Self.Iterator.Element,
+    option: NSBinarySearchingOptions = NSBinarySearchingOptions.firstEqual
+    ) -> Index?
+  {
+    return indexOfBoundedObject(option, equality: { $0 == object }, greaterThan: { $0 > object })
+  }
+
+  fileprivate func indexOfInsertion(_ object: Self.Iterator.Element) -> Index {
+    return indexOfInsertion({ $0 == object }, greaterThan: { $0 > object }, lessThan: { $0 < object })
+  }
+
+  // MARK: Private
+
+  fileprivate func indexOfBoundedObject(
+    _ option: NSBinarySearchingOptions = NSBinarySearchingOptions.firstEqual,
+    equality: Comparator,
+    greaterThan: Comparator
+    ) -> Index?
+  {
+    var lo = self.startIndex
+    var hi = self.endIndex
+    var best: Index?
+
+    while lo <= hi { // vip!
+      let mid = (hi - lo) / 2 + lo
+
+      if mid >= count {
+        break
+      }
+
+      if equality(self[mid]) {
+        best = mid
+
+        switch option {
+        case NSBinarySearchingOptions.firstEqual:
+          // try lower
+          hi = mid - 1
+        case NSBinarySearchingOptions.lastEqual:
+          // try higher
+          lo = mid + 1
+        default:
+          break
+        }
+      } else if greaterThan(self[mid]) {
+        // try lower
+        hi = mid - 1
+      } else {
+        // try higher
+        lo = mid + 1
+      }
+    }
+
+    return best
+  }
+
+  fileprivate func indexOfInsertion(
+    _ equality: Comparator,
+    greaterThan: Comparator,
+    lessThan: Comparator
+    ) -> Index
+  {
+    if count == 0 {
+      return 0
+    }
+
+    var lo = self.startIndex
+    var hi = self.endIndex
+    var mid: Index = 0
+
+    while lo < hi {
+      mid = (hi - lo) / 2 + lo
+      let val = self[mid]
+
+      if equality(val) {
+        return mid
+      }
+      else if greaterThan(val) {
+        // search low
+        hi = mid
+      }
+      else {
+        // search hi
+        lo = mid + 1
+      }
+    }
+
+    return lessThan(self[mid]) ? mid + 1 : mid
+  }
+}
+
+extension Array where Element: Comparable, Element: Equatable
+{
+  mutating func insertAtSortedInsertionPoint(_ object: Iterator.Element) -> Index {
+    let index = indexOfInsertion(object)
+    insert(object, at: index)
+    return index
+  }
+}

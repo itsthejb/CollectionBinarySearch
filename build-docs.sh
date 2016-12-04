cd8d4ab9740a47f73c6ebb8c7fb7e103b9c38c25
Jonathan Crooke <joncrooke@gmail.com>
2016-12-04 21:02:24 +0100

    Update author

--- build-docs.sh
+++ build-docs.sh
@@ -2,7 +2,7 @@
 
 jazzy \
   --clean \
-  --author Jon Crooke \
+  --author "Jonathan Crooke" \
   --github_url https://github.com/itsthejb/CollectionBinarySearch \
   --module-version 1.0 \
   --xcodebuild-arguments -scheme,CollectionBinarySearch \
